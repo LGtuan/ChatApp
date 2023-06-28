b634d04c1617906cbd0b7dba5c2443fcc852f35f
LGtuan <tuanlgph20769@fpt.edu.vn>
2023-06-28 17:08:58 +0700

    auth ui

--- FrontEnd/app/src/main/java/com/example/frontend/MainActivity.java
+++ FrontEnd/app/src/main/java/com/example/frontend/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.frontend.activity.LoginActivity;
+import com.example.frontend.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonShowLogin,buttonShowRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonShowLogin = findViewById(R.id.btn_show_sign_in);
+        buttonShowRegister = findViewById(R.id.btn_show_register);
+
+        buttonShowLogin.setOnClickListener(v ->{
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        buttonShowRegister.setOnClickListener(v-> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
